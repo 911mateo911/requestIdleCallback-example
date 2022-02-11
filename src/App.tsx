f5a3be1de569f7028146ca398bc8124f79042fef
911mateo911 <malajmateo@gmail.com>
2022-02-11 15:11:43 +0100

    with fetch

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,36 @@ import { doPointlessComputationsWithBlocking } from './blockThread';
 function App() {
     const [amount, setAmount] = useState(10000)
 
+    /**
+     * [line 28] 1 (no shcheduling)
+     * [line 40] 3 (no shcheduling)
+     * [line 44] painted (no shcheduling)
+     * [line 51] scheduled task (no shcheduling)
+     * 
+     * [line 39] hello (timeout 1)
+     * [line 46] 2, computations (timeout 1)
+     * [line 50] hello after paint (timeout 10)
+     * 
+     * [line 30] fetch (timeout 100)
+     * 
+     * [line 35] 1 computations (timeout 1000)
+     * 
+     * [line 49] before block (timeout 1000)
+     */
+
     useLayoutEffect(() => {
         console.log('1')
+        Scheduler.scheduleTask(async () => {
+            const haha = await fetch('https://jsonplaceholder.typicode.com/todos/');
+
+            console.log(await haha.json())
+        }, 100)
         Scheduler.scheduleTask(() => {
             console.log('1')
             doPointlessComputationsWithBlocking();
-        }, 0)
+        }, 1000)
         // setInterval(() => console.log('frominterval'), 10)
-        Scheduler.scheduleTask(() => console.log('hello'), 1000);
+        Scheduler.scheduleTask(() => console.log('hello'), 1);
         console.log('3')
     }, [])
 
@@ -24,7 +46,7 @@ function App() {
             console.log('2')
             doPointlessComputationsWithBlocking();
         }, 1)
-        Scheduler.scheduleTask(() => console.log('before block'), 1)
+        Scheduler.scheduleTask(() => console.log('before block'), 1000)
         Scheduler.scheduleTask(() => console.log('hello after paint'), 10);
         console.log('scheduled task');
     }, [])
